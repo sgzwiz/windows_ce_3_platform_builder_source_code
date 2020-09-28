c6b20b5b0841d6fe17e3f3c347b6bc8374ee9b68
AUnicornWithNoLife <benjamin.barratt@icloud.com>
2020-09-28 20:59:55 +0100

    a

--- windows_ce_4_2_081231/WINCE420/PRIVATE/TEST/DRIVERS/PARALLEL/API/PARTEST/readme.txt
+++
@@ -1,3 +0,0 @@
-the parallel port API test for windows CE.
-
-Read the test plan and setup docs for setup details.
--- windows_ce_4_2_081231/WINCE420/PRIVATE/WINCEOS/COREOS/NK/TOOLS/ROMIMAGE/DIFFBIN/reloc.h
+++
@@ -1,8 +0,0 @@
-// reloc.h
-#pragma once
-
-BOOL
-RelocateImageFile(CModuleData *pOldMod, CModuleData *pNewMod, LPCSTR szFilename);
-
-HRESULT
-ApplyReverseFixups(CImageData *pimgOld, CImageData *pimgNew);
